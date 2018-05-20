21e1a96dcffc56a62c6374ff31ef6335994d2f9a
philipkovachev <philipkovachev9@yahoo.com>
2018-05-20 12:52:31 +0300

    W3Schools Python Strings Literals Tutorial

---
+++ Math Concepts For Developers/Python W3 Schools Tutorial/String Literals.py
@@ -0,0 +1,30 @@
+#S simple string:
+
+a = "hello"
+print(a[1]);
+
+#Substrings:
+#Printing the full string
+
+b = "world";
+print(b[0:5]);
+
+#len() method:
+
+a = "Hello World";
+print(len(a));
+
+#strip() method:
+print(a.strip());
+
+#lower() method:
+print(a.lower());
+
+#upper() method:
+print(a.upper());
+
+#replace method():
+print(a.replace("H", "J"));
+
+#split() method:
+print(a.split(","));
