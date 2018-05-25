2591d3280760e70415fbc6a804a25c6f58906c88
philipkovachev <philipkovachev9@yahoo.com>
2018-05-25 13:24:46 +0300

    Odd numbers task solved

---
+++ JavaScript Fundamentals/Arrays and Matrices/Process Odd Numbers.js
@@ -0,0 +1,13 @@
+/*
+You are given an array of numbers, print the odd numbers, doubled and reversed ones.
+*/
+
+function print(array) {
+    console.log(
+      array.filter((element, index) => element % index === 1)
+      .map(a => a * 2)
+      .reverse()
+      .join(' '))
+      
+   }
+   print([10,15,20,25])
