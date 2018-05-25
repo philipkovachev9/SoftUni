003925a052408c7e1982d792c0b447f8a901aa48
philipkovachev <philipkovachev9@yahoo.com>
2018-05-25 13:02:41 +0300

    First and Last K Numbers Task solved

---
+++ JavaScript Fundamentals/Arrays and Matrices/First and Last K Numbers.js
@@ -0,0 +1,15 @@
+/*
+Write a JS function that prints the first k and the last k elements from an array of numbers.
+The input comes as array of number elements. The first element represents the number k, all other elements are
+from the array that needs to be processed.
+The output is printed on the console on two lines. On the first line print the first k elements, separated by space. On
+the second line print the last k elements, separated by space.
+*/
+function firstAndLastNumbers(array) {
+    let k = array.shift();
+    console.log(array.slice(0,k).join(' '));
+    console.log(array.slice(array.length - k).join(' '));
+     
+  }
+  
+  firstAndLastNumbers([2,7,8,9]);
