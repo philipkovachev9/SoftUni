a6d40c6d7057716b54a0d10aeff4a56f4cdaa837
Philip <philipkovachev9@yahoo.com>
2018-09-22 11:14:25 +0300

    Users can now be loaded from the database

---
+++ Java Script Applications/Ajax and JQuery Ajax/4. Load data from Firebase/phonebook.js
@@ -0,0 +1,26 @@
+/* Create a database on firebase and retrieve the data from there. Also make sure
+that the user can add new people to the phonebook and delete them
+*/
+
+const URL = 'https://test-app-8058c.firebaseio.com/phonebook/phonebook';
+const TABLE = $('#phonebook');
+
+$('#btnLoad').on('click', loadContacts);
+
+function loadContacts() {
+    $.ajax({
+        method: 'GET',
+        url: URL + '.json'
+    }).then(appendContacts).catch(handleError)
+}
+
+function appendContacts(contacts) {
+    for (const key in contacts) {
+      let li = $(`<li>${contacts[key].name}: ${contacts[key].phone} </li>`)
+      TABLE.append(li);
+    }
+}
+
+function handleError(error) {
+    console.log(error);
+}
