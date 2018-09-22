17b1b50444b6945440d7f9aec8860f5847ed7c47
Philip <philipkovachev9@yahoo.com>
2018-09-22 11:15:47 +0300

    Changed the formatting

--- Java Script Applications/Ajax and JQuery Ajax/4. Load data from Firebase/phonebook.js
+++ Java Script Applications/Ajax and JQuery Ajax/4. Load data from Firebase/phonebook.js
@@ -16,7 +16,7 @@ function loadContacts() {
 
 function appendContacts(contacts) {
     for (const key in contacts) {
-      let li = $(`<li>${contacts[key].name}: ${contacts[key].phone} </li>`)
+      let li = $(`<li>Name: ${contacts[key].name}: Phone ${contacts[key].phone} </li>`)
       TABLE.append(li);
     }
 }
