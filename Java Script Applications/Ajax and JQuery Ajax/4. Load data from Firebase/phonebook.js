6f5986b25b508bd7f587ed15441f1b237095bbb1
Philip <philipkovachev9@yahoo.com>
2018-09-23 10:21:43 +0300

    Items can now be deleted from the database

--- Java Script Applications/Ajax and JQuery Ajax/4. Load data from Firebase/phonebook.js
+++ Java Script Applications/Ajax and JQuery Ajax/4. Load data from Firebase/phonebook.js
@@ -4,8 +4,12 @@ that the user can add new people to the phonebook and delete them
 
 const URL = 'https://test-app-8058c.firebaseio.com/phonebook/phonebook';
 const TABLE = $('#phonebook');
+const NAME = $('#person');
+const PHONE = $('#phone');
 
 $('#btnLoad').on('click', loadContacts);
+$('#btnLoad').on('click', createContact)
+
 
 function loadContacts() {
     $.ajax({
@@ -15,9 +19,35 @@ function loadContacts() {
 }
 
 function appendContacts(contacts) {
+    TABLE.empty();
     for (const key in contacts) {
-      let li = $(`<li>Name: ${contacts[key].name}: Phone ${contacts[key].phone} </li>`)
-      TABLE.append(li);
+        let li = $('<li>');
+        let button = $('<button>Delete</button>');
+        button.on('click', function() {
+            $.ajax({
+                method: 'DELETE',
+                url: URL + '/' + this.id + '.json'
+            }). then(function() {
+                li.remove();
+            }).catch(handleError)
+        })
+        li.text(`Name: ${contacts[key].name}: Phone ${contacts[key].phone}`)
+        li.append(button);
+        TABLE.append(li);
+    }
+}
+
+function createContact() {
+    let name = NAME.val();
+    let phone = PHONE.val();
+    if (name.trim() !== '' && phone.trim() !== '') {
+        $.ajax({
+            method: 'POST',
+            url: URL + '.json',
+            data:JSON.stringify({name, phone})
+        }).then(function (res) {
+            console.log(res)
+        }).catch(handleError)
     }
 }
 
